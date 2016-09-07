49b14e7cbb1da4fd422eef2031216a66c5cad1da
kntmrkm <kntmrkm@gmail.com>
2016-09-07 10:50:39 +0900

    same TargetApi OK

--- app/src/main/java/jp/webta/testnotificationlistenerservice/service/MyNotificationListenerService.java
+++ app/src/main/java/jp/webta/testnotificationlistenerservice/service/MyNotificationListenerService.java
@@ -1,5 +1,6 @@
 package jp.webta.testnotificationlistenerservice.service;
 
+import android.annotation.TargetApi;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
@@ -7,9 +8,11 @@ import android.util.Log;
 /**
  * Created by kntmrkm on 16/09/07.
  */
+@TargetApi(18)
 public class MyNotificationListenerService extends NotificationListenerService {
     private static final String TAG = "MyNotificationA";
 
+    @TargetApi(18)
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
         Log.d(TAG, "onNotificationPosted: " + sbn.toString());
